b21a3f465695cbd842c27584c91b3e576715f0c9
Michael Anderson <33232231+MichaelAceAnderson@users.noreply.github.com>
2024-10-29 12:45:43 +0100

    Initial commit

---
+++ src/game/type/breakout/Breakout.java
@@ -0,0 +1,240 @@
+package game.type.breakout;
+
+ 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+import display.view.GameFrame;
+import game.Game;
+import game.type.breakout.entities.Entity.Direction;
+import game.type.breakout.entities.type.Ball;
+import game.type.breakout.entities.type.Brick;
+import game.type.breakout.entities.type.Player;
+
+public class Breakout extends Game{
+	public final static String ASSETS_PATH = System.getProperty("user.dir") + File.separator + "src" + File.separator 
+	+ "game" +  File.separator + "type" + File.separator + "breakout" + File.separator + "assets" + File.separator;
+
+	private ArrayList<Brick> bricks;
+	private Player player;
+	private Ball ball;
+
+	/**
+	 * Instantiates a new Breakout game
+	 * 
+	 * @param gameFrame The frame in which the game is displayed
+	 */
+	public Breakout(GameFrame gameFrame) {
+		super(gameFrame.getGamePanel(), "Breakout");
+		this.setBricks(new ArrayList<Brick>());
+		this.setPlayer(new Player(630,700));
+		this.setBall(new Ball(Ball.DEFAULT_IMAGE, 630,600, 20));
+
+		KeyListener keyListener = new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_Q:
+					case KeyEvent.VK_LEFT:
+						Breakout.this.getPlayer().setDirection(Direction.LEFT);
+						break;
+					case KeyEvent.VK_D:
+					case KeyEvent.VK_RIGHT:
+						Breakout.this.getPlayer().setDirection(Direction.RIGHT);
+						break;
+					case KeyEvent.VK_ESCAPE:
+						if(Breakout.this.isPaused()){
+							Breakout.this.resume();
+						}
+						else{
+							Breakout.this.pause();
+						}
+						break;
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_Q:
+					case KeyEvent.VK_LEFT:
+						Breakout.this.getPlayer().setDirection(Direction.NONE);
+						break;
+					case KeyEvent.VK_D:
+					case KeyEvent.VK_RIGHT:
+						Breakout.this.getPlayer().setDirection(Direction.NONE);
+						break;
+				}
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		};
+		// Make the panel focusable so it can listen to key inputs
+		this.getPanel().requestFocus();
+		this.getPanel().addKeyListener(keyListener);
+	}
+
+
+	/**
+	 * Get the list of bricks in the game.
+	 * 
+	 * @return The list of bricks
+	 */
+	public ArrayList<Brick> getBricks() {
+		return this.bricks;
+	}
+
+	/**
+	 * Set the list of bricks in the game.
+	 * 
+	 * @param bricks The list of bricks
+	 */
+	public void setBricks(ArrayList<Brick> bricks) {
+		this.bricks = bricks;
+	}
+
+	/**
+	 * Get the player entity in the game.
+	 * 
+	 * @return The player entity
+	 */
+	public Player getPlayer() {
+		return this.player;
+	}
+
+	/**
+	 * Set the player entity in the game.
+	 * 
+	 * @param player The player entity
+	 */
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	/**
+	 * Get the ball entity in the game.
+	 * 
+	 * @return The ball entity
+	 */
+	public Ball getBall() {
+		return this.ball;
+	}
+
+	/**
+	 * Set the ball entity in the game.
+	 * 
+	 * @param ball The ball entity
+	 */
+	public void setBall(Ball ball) {
+		this.ball = ball;
+	}
+
+	/**
+	 * Initializes bricks in a level
+	 * 
+	 * @param rows The number of rows of bricks
+	 * @param columns The number of columns of bricks
+	 */
+	public void createBricks(int rows, int columns){
+		// TODO: Prevent the amount of bricks from exceeding the panel's width and height
+		// See GraphicalObject#isOnScreen(x, y, panel)
+
+		final int BRICK_SPACING = Brick.DEFAULT_WIDTH + 10;
+
+		// Start the bricks at the center of the panel
+		int initialXPos = (int) Math.floor(this.getPanel().getPreferredSize().getWidth()
+		/ 2 - (columns * BRICK_SPACING) / 2);
+		
+		for(int row = 0; row < rows; row++){
+			for(int column = 0; column < columns; column++){
+				int verticalPos = Brick.DEFAULT_POS_Y + row * (Brick.DEFAULT_HEIGHT + 10);
+				int randomLifespan = new Random().nextInt(Brick.MAX_LIFESPAN);
+
+				this.getBricks().add(new Brick(initialXPos+column*BRICK_SPACING,verticalPos,
+				Brick.DEFAULT_WIDTH,Brick.DEFAULT_HEIGHT,
+				randomLifespan, false));
+			}
+		}
+	}
+
+	/**
+	 * @see game.Game#start()
+	 */
+	@Override
+	public void start() {
+		super.start();
+		this.createBricks(6, 10);
+
+		// Add all entities to the game
+		for (Brick brick : this.getBricks()) {
+			this.getPanel().add(brick.getRepresentation());
+		}
+		this.getPanel().add(this.getPlayer().getRepresentation());
+		this.getPanel().add(this.getBall().getRepresentation());
+		this.getBall().setDirection(Direction.UP);
+	}
+
+	/**
+	 * Update the player entity
+	 */
+	public void updatePlayer() {
+		if(!this.getPlayer().willBeOffScreen(this.getPanel(), Player.MOVE_SPEED)){
+			this.getPlayer().move(Player.MOVE_SPEED);
+
+		}
+	}
+
+	/**
+	 * Update the ball entity
+	 */
+	public void updateBall() {
+		if(this.getBall().willBeOffScreen(this.getPanel(), Ball.MOVE_SPEED)
+		|| this.getBall().getRepresentation().isColliding(this.getPlayer().getRepresentation())){
+			this.getBall().reverseDirection();
+		}
+		this.getBall().move(Ball.MOVE_SPEED);
+	}
+
+	/**
+	 * Update the bricks entities
+	 */
+	public void updateBricks() {
+		// Using an iterator to safely remove bricks from the collection
+		// Without getting the ConcurrentModificationException
+		Iterator<Brick> iterator = this.getBricks().iterator();
+
+		while (iterator.hasNext()) {
+			Brick brick = iterator.next();
+			if (brick.getRepresentation().isColliding(this.getBall().getRepresentation())) {
+				this.getBall().reverseDirection();
+				if (brick.getLifespan()-1 < Brick.MIN_LIFESPAN) {
+					this.getPanel().remove(brick.getRepresentation());
+					// Safely remove the brick from the collection
+					iterator.remove(); 
+				}
+				else{
+					brick.setLifespan(brick.getLifespan() - 1);
+				}
+				// Break the loop to prevent the ball from colliding with multiple bricks
+				// and avoid the multiple reverseDirection() calls (making the ball continue in the same direction)
+				break;
+			}
+		}
+	}
+
+	/**
+	 * @see game.Game#onUpdate()
+	 */
+	@Override
+	public void onUpdate() {
+		this.updatePlayer();
+		this.updateBall();
+		this.updateBricks();
+	}
+}
